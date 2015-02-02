5cf43f5e4b0fde8fefe4817ef8d4f7a502af0d2d
mariano <mariano@mariano-MacBookAir.(none)>
2015-02-02 11:42:48 +0100

    ejercicio resuelto

---
+++ calculator-random.js
@@ -0,0 +1,24 @@
+
+function calculator(){
+	var op = document.getElementById("op");
+	var sentence = op.innerHTML;
+	var res = sentence.split("+");
+	var a = parseInt(res[0]);
+	var b = parseInt(res[1]);
+	var result = a + b;
+	var final = result.toString(); 
+	changer("res", final);
+
+}
+
+function changer (id, newValue) {
+	var element = document.getElementById(id);
+	element.innerHTML = "= "+newValue;
+}
+
+function calcRandom(){
+	var numAleatorio1 = Math.floor(Math.random()*1000);
+	var numAleatorio2 = Math.floor(Math.random()*1000);
+	var op = document.getElementById("op");
+	op.innerHTML = numAleatorio1.toString() + " + " + numAleatorio2.toString();
+}
